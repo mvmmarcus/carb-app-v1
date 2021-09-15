1b6bfb3759c1694612e5da072c310e2a7e5a1ba7
Marcus Vinicius Marques <marcus.marques@wisereducacao.com>
2021-09-15 05:17:27 -0300

    feat: add initial configs

---
+++ src/pages/Settings/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Container, Text} from '../Settings/styles';
+
+const SettingsPage = () => {
+  return (
+    <Container>
+      <Text>Settings</Text>
+    </Container>
+  );
+};
+
+export default SettingsPage;
---
+++ src/pages/Settings/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+`;
+
+export const Text = styled.Text`
+  color: black;
+`;
